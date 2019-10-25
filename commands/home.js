0a45b77ec0c551190a93380c12ca424474494c3f
Joshua Baert <developer@baert.io>
2019-10-25 09:54:00 -0600

    save

--- commands/home.js
+++ commands/home.js
@@ -1,12 +1,17 @@
 module.exports = async function (player, args) {
     if (args[0] === 'set') {
-        let playerInfo = await this.getEntityForPlayer(player);
-        this.playerHomes[player] = { Pos: playerInfo.Pos, Rot: playerInfo.Rotation };
-        this.writeToMine(`w ${player} Setting your home to ${playerInfo.Pos.join(' ')}`);
+        // Get Position & Rotation
+        let position = await this.getPlayerPosition(player);
+        let rotation = await this.getPlayerRotation(player);
+
+        // Saving players position and rotation for use later
+        this.playerHomes[player] = { pos: position, rot: rotation };
+        this.writeToMine(`w ${player} Setting your home to ${position.join(' ')}`);
     } else if (args.length === 0) {
+        // grab and see if players home exists
         let playerHome = this.playerHomes[player];
         if (playerHome) {
-            this.writeToMine(`tp ${player} ${playerHome.Pos.join(' ')} ${playerHome.Rot.join(' ')}`);
+            this.writeToMine(`tp ${player} ${playerHome.pos.join(' ')} ${playerHome.rot.join(' ')}`);
         } else {
             this.writeToMine(`w ${player} Your home is not set yet.`);
         }
--- server.js
+++ server.js
@@ -75,57 +75,77 @@ module.exports = class Server {
 
     }
 
-    getEntityForPlayer(player) {
-        return new Promise((resolve) => {
-            let entity;
+    parseEntityData(rawString) {
+        // Converting data structure to Javascript.
+        let entityStr = rawString.split(' ').map((t) => {
+            // ints and bools
+            if (/\d+b/.test(t)) return t.replace(/(\d+)b/, '$1');
 
-            const listenForEntity = (data) => {
+            // seconds to number
+            let secondsReg = /(\d+)s/;
+            if (secondsReg.test(t)) return t.replace(secondsReg, '$1');
+
+            //floats / doubles
+            if (/\d+\.\d*(d|f)/.test(t)) {
+                return t.split('.')
+                    .map((str, i) => {
+                        if (i === 1) {
+                            return str.replace(/(.*)(\d+)(.*)/, '$1 $2 $3')
+                                .split(' ')
+                                .map(d => /\d+/.test(d) ? d.substr(0, 3) : d)
+                                .join('');
+                        }
+                        return str;
+                    }).join('.')
+                    .replace(/d|f/, '');
+            }
+
+            // Weird L?
+            if (/(-{0,1}\d+)L/.test(t)) return t.replace(/(-{0,1}\d+)L/, '"$1"');
+
+            // Default
+            return t;
+        }).join(' ');
+
+        return eval(`(${entityStr})`);
+    }
+
+    getPlayerPosition(player) {
+        return new Promise((resolve) => {
+            const listenForPosition = (data) => {
                 let text = data.toString();
 
                 if (!(/has\sthe\sfollowing\sentity\sdata:/).test(text)) return;
-                this.serverProcess.stdout.removeListener('data', listenForEntity);
+                this.serverProcess.stdout.removeListener('data', listenForPosition);
+                let rawEntityText = text.split('entity data: ')[1];
+                let position = this.parseEntityData(rawEntityText);
 
-                let entityStrRaw = text.split('entity data: ')[1];
-
-                // Converting data structure to Javascript.
-                let entityStr = entityStrRaw.split(' ').map((t) => {
-                    // ints and bools
-                    if (/\d+b/.test(t)) return t.replace(/(\d+)b/, '$1');
-
-                    // seconds to number
-                    let secondsReg = /(\d+)s/;
-                    if (secondsReg.test(t)) return t.replace(secondsReg, '$1');
-
-                    //floats / doubles
-                    if (/\d+\.\d*(d|f)/.test(t)) {
-                        return t.split('.')
-                            .map((str, i) => {
-                                if (i === 1) {
-                                    return str.replace(/(.*)(\d+)(.*)/, '$1 $2 $3')
-                                        .split(' ')
-                                        .map(d => /\d+/.test(d) ? d.substr(0, 3) : d)
-                                        .join('');
-                                }
-                                return str;
-                            }).join('.')
-                            .replace(/d|f/, '');
-                    }
-
-                    // Weird L?
-                    if (/(-{0,1}\d+)L/.test(t)) return t.replace(/(-{0,1}\d+)L/, '"$1"');
-
-                    // Default
-                    return t;
-                }).join(' ');
-                entity = eval(`(${entityStr})`);
-                this.playerEntities[player] = entity;
-
-                resolve(entity);
+                resolve(position);
             };
 
-            this.serverProcess.stdout.on('data', listenForEntity);
-            this.writeToMine(`data get entity ${player}`);
+            this.serverProcess.stdout.on('data', listenForPosition);
+
+            this.writeToMine(`data get entity ${player} Pos`);
         });
-    };
+    }
+
+    getPlayerRotation(player) {
+        return new Promise((resolve) => {
+            const listenForRotation = (data) => {
+                let text = data.toString();
+
+                if (!(/has\sthe\sfollowing\sentity\sdata:/).test(text)) return;
+                this.serverProcess.stdout.removeListener('data', listenForRotation);
+                let rawEntityText = text.split('entity data: ')[1];
+                let rotation = this.parseEntityData(rawEntityText);
+
+                resolve(rotation);
+            };
+
+            this.serverProcess.stdout.on('data', listenForRotation);
+
+            this.writeToMine(`data get entity ${player} Rotation`);
+        });
+    }
 
 };
