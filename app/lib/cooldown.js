bdae398aac709409153305910a24acc8812c3639
Harrison King <authorharrisonking@gmail.com>
2019-11-12 20:04:20 -0700

    beginning to code something

--- app/lib/cooldown.js
+++ app/lib/cooldown.js
@@ -6,13 +6,62 @@ module.exports = Base => class extends Base {
     }
     
 
-    checkCooldown(playerName, command, startCooldown) {
+    checkCooldown(playerName, command, time) {
         console.log('hello world');
+        //make sure command is valid. currently hardcoded to only accept !home and !warp, likely we'll need change this sooner than later.
+        validCommand => {
+            if (command.toLowerCase() !== "!home" || "!warp") return;
+        
+        //array of commands currently on cooldown.
+        let onCooldown = [];
+
+        //checking if command is in the array and responding accordingly.
+        if (onCooldown.some(i => i == command) {
+            this.whisperPlayerRaw(args[0], [
+                { text: `${command} is on cooldown for `, color: 'white' },
+                { text: `${time}`, color: 'red' },
+                { text: ` minutes.`, color: 'white' },
+                ]);
+        }
     }
 
     startCooldown(playerName, command, time) {
     }
 };
+
+
+
+
+
+
+
+// {
+//     if (args[0] && args[0].toLowerCase() === 'accept') {
+//         this.handleWarpAccept(playerName);
+//     } else if (this.loggedInPlayers.indexOf(args[0]) !== -1) {
+//         // If the first word is a players name then make a request for warp
+//         this.whisperPlayerRaw(args[0], [
+//             { text: `Do you want to accept warp from ${playerName}? \nType `, color: 'white' },
+//             { text: `!warp accept`, color: 'green' },
+//             { text: ` to accept`, color: 'white' },
+//         ]);
+//         this.warpRequests[args[0]] = playerName;
+//     } else {
+//         // they got here because they messed up
+//         if (!args[0]) {
+//             this.whisperPlayer(playerName, 'You need to target a player.', 'red');
+//             // this.writeToMine(`w ${playerName} You need to target a player`);
+//         } else {
+//             this.whisperPlayerRaw(playerName, [
+//                 { text: `Player `, color: 'white' },
+//                 { text: `${args[0]}`, color: 'aqua' },
+//                 { text: ` is not logged in. \nDid you mean to type `, color: 'white' },
+//                 { text: `!warp accept`, color: 'green' },
+//                 { text: `?`, color: 'white' },
+//             ]);
+//         }
+//     }
+// }
 
 //goals: a cooldown object that can be added to any object with arguements that set length of cooldown,
 //add the object to the home command and possibly the warp command.
