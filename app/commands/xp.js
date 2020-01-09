0ab897e0c1ab15fed2d5551513900055fb144520
Harrison King <authorharrisonking@gmail.com>
2020-01-08 17:26:16 -0700

    got some bones up

--- app/commands/xp.js
+++ app/commands/xp.js
@@ -4,50 +4,64 @@ module.exports = Base => class extends Base {
         
     }
 
-    async handleXp(playerName, args) {
-        // Check to see if the player is in the end if so don't allow anything home related
-        // I could have checked upon request if the recipient is in the end but decided it wasn't worth the extra code
-        if ((await this.getPlayerDimension(playerName)) === 'minecraft:the_end') {
-            return this.whisperPlayer(playerName, 'Sorry can not use the warp command in the end', 'red');
-        }
-
-        let loggedInPlayers = await this.getListOfOnlinePlayers();
-
-        if (args[0] && args[0].toLowerCase() === 'accept') {
-            this.handleWarpAccept(playerName);
-        } else if (loggedInPlayers.indexOf(args[0]) !== -1) {
-            // If the first word is a players name then make a request for warp
-
-            //cooldownCheck goes here
-            if (this.cooldownCheck('warp', playerName) == true) return;
-
-            this.whisperPlayerRaw(playerName, ['Sent warp request to ', { text: args[0], color: 'green' }]);
-
-
-            this.whisperPlayerRaw(args[0], [
-                { text: `Do you want to accept warp from ${playerName}? \nType `, color: 'white' },
-                { text: `!warp accept`, color: 'green' },
-                { text: ` to accept`, color: 'white' },
-            ]);
-            this.warpRequests[args[0]] = playerName;
-        } else {
-            // they got here because they messed up
-            if (!args[0]) {
-                this.whisperPlayer(playerName, 'You need to target a player.', 'red');
-                // this.writeToMine(`w ${playerName} You need to target a player`);
-            } else {
-                this.whisperPlayerRaw(playerName, [
-                    { text: `Player `, color: 'white' },
-                    { text: `${args[0]}`, color: 'aqua' },
-                    { text: ` is not logged in. \nDid you mean to type `, color: 'white' },
-                    { text: `!warp accept`, color: 'green' },
-                    { text: `?`, color: 'white' },
-                ]);
+    handleXp(playerName, args) {
+    //choose a function based on the args.    
+        (() => {
+            switch (args[0].toLowerCase()) {
+            case 'store':
+                return this.handleXpStore(playerName, args.slice(1));
+            case 'get':
+                return this.handleXpGet(playerName, args.slice(1));
+            case 'check':
+                return this.handleXpCheck(playerName);
+            default:
+                //they got here by typing the wrong thing, will list the things they can type.
+                return this.whisperPlayerRaw(playerName, [
+                    { text: `To store all XP, type `, color: 'white' },
+                    { text: `!xp store`, color: 'green' },
+                    { text: `.\nTo retrieve all stored XP, type `, color: 'white' },
+                    { text: `!xp get`, color: 'green' },
+                    { text: `.\nYou can also store and retieve partial amounts of XP by appending a `, color: 'white' },
+                    { text: `number`, color: 'red' },
+                    { text: ` to the command.\nEG: `, color: 'white' },
+                    { text: `!xp get 3`, color: 'green' },
+                    { text: ` will retrieve 3 XP levels.\nType `, color: 'white' },
+                    { text: `!xp check`, color: 'green' },
+                    { text: ` to check how many XP levels you have stored.`, color: 'white' },
+                ]);;
             }
-        }
+        })();
     }
 
-    getPlayerXp() {
+    handleXpStore(playerName, ...args) {
+        //did player append a number to the command?
+        if (!args[0]) {
+
+        } else {
+
+        }
+
+    }
+
+    handleXpGet(playerName, ...args) {
+        //did player append a number to the command?
+        if (!args[0]) {
+
+        } else {
+            
+        }
+
+    }
+
+    handleXpCheck(playerName) {
+
+    }
+
+    getPlayerXp(level) {
+
+    }
+
+    XpFromDatabase(level) {
 
     }
 }
@@ -76,5 +90,5 @@ write-to-mine that player's new xp level is current player level + var level
 
 !xp check
 set var level to database
-write-to-mine message: You have level XP points stored.
+whisperPlayerRaw message: You have level XP points stored.
 */
