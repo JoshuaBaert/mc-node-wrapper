6cef935944c5c7071af68ebc7973fb8108af9b42
Harrison King <authorharrisonking@gmail.com>
2020-02-12 15:52:25 -0700

    handlewarprequest ready working on accept

--- app/commands/warp.js
+++ app/commands/warp.js
@@ -32,18 +32,18 @@ module.exports = Base => class extends Base {
         if ((await this.getPlayerDimension(playerName)) === 'minecraft:the_end') {
             return this.tellPlayer(playerName, 'Sorry can not use the warp command in the end', 'red');
         }
-
-        let warpTo;
-        let loggedInPlayers = await this.getListOfOnlinePlayers();
-        if (loggedInPlayers.indexOf(args[0]) !== -1) {
-            // If the first word is a players name then make a request for warp
-            warpTo = loggedInPlayers.indexOf(args[0]);
-        }
         
         //choose a function based on the args.   
         if (!args[0]) {
             return this.handleWrongWarpInput(playerName);
         } else {
+            let warpTo;
+            let loggedInPlayers = await this.getListOfOnlinePlayers();
+            if (loggedInPlayers.indexOf(args[0]) !== -1) {
+                // If the first word is a players name then make a request for warp
+                warpTo = loggedInPlayers.indexOf(args[0]);
+            }
+
             (() => {
                 switch (args[0].toLowerCase()) {
                 case `${warpTo}`:
@@ -68,28 +68,41 @@ module.exports = Base => class extends Base {
             { text: `Type `, color: 'white' },
             { text: `!help warp`, color: 'green' },
             { text: ` for a list of commands.\n`, color: 'white' },
-            { text: `If you intended to type the name of a player, check to make sure they're logged in, and that it's spelled right.`, color: 'white' },
+            { text: `If you meant to type the name of a player, make sure they're logged in, and that it's spelled right.`, color: 'white' },
         ]);
     }
 
-    async handleWarpRequest(playerName, warpTo) {
-        
+    async handleWarpRequest(playerName, warpTo) {  
         //cooldownCheck goes here
         if (this.cooldownCheck('warp', playerName) == true) return;
 
-        this.tellPlayerRaw(playerName, ['Sent warp request to ', { text: warpTo, color: 'green' }]);
+        //if player is not already on warpTo's queue:
+        if (this.addToQueue(warpTo, playerName) == false) {
+            this.tellPlayerRaw(playerName, ['Sent warp request to ', { text: warpTo, color: 'green' }]);
 
+            this.tellPlayerRaw(warpTo, [
+                { text: `Do you want to accept warp from ${playerName}? \nType `, color: 'white' },
+                { text: `!warp accept`, color: 'green' },
+                { text: ` to accept`, color: 'white' },
+            ]);
+            return;
+        }
 
-        this.tellPlayerRaw(warpTo, [
-            { text: `Do you want to accept warp from ${playerName}? \nType `, color: 'white' },
-            { text: `!warp accept`, color: 'green' },
-            { text: ` to accept`, color: 'white' },
-        ]);
-        this.warpRequests[warpTo] = playerName;
+        //player is already on queue
+        this.tellPlayer(requestingPlayer, `Already sent a request to that player.`, 'red');  
     }
 
     handleWarpAccept(playerName, args) {
-        // if it is accept see if there is a player that requested a warp
+        //instantiate warp queue if it hasn't been instantiated yet.
+        if (!this.warpRequests[playerName]) this.warpRequests[playerName] = [];
+        
+        // if no players on the queue
+        if (this.warpRequests[playerName].length === 0) {
+            this.tellPlayer(playerName, `No pending warp requests.`, 'red');
+            return;
+        }
+
+        //needs work\/\/
         // then if there is warp them to the accepting player
         let requestingPlayer = this.warpRequests[playerName];
         if (requestingPlayer) {
@@ -101,9 +114,7 @@ module.exports = Base => class extends Base {
             //cooldownStart goes here.
             this.cooldownStart('warp', requestingPlayer);
 
-        } else {
-            this.tellPlayer(playerName, `No pending warp requests.`, 'red');
-        }
+        } 
     }
 
     handleWarpDecline(playerName, args) {
@@ -126,5 +137,20 @@ module.exports = Base => class extends Base {
 
     handleWarpQueue(playerName) {
 
+    }
+
+    addToQueue(acceptingPlayer, requestingPlayer) {
+        //instantiate warp queue if it hasn't been instantiated yet.
+        if (!this.warpRequests[acceptingPlayer]) this.warpRequests[acceptingPlayer] = [];
+
+        //if player is NOT already on queue...
+        if (this.warpRequests[acceptingPlayer].indexOf(requestingPlayer) === -1) {
+            //...push requesting player to back of queue
+            this.warpRequests[acceptingPlayer].push(requestingPlayer);
+            return false;
+        }
+
+        //if player is on queue
+        return true;
     }
 };
