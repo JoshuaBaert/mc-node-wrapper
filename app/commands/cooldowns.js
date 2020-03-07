0fa7b1f680396eac1d65f836bf3b3a8abd31bd5a
Harrison King <authorharrisonking@gmail.com>
2020-03-07 15:17:24 -0700

    works, sending to pr

--- app/commands/cooldowns.js
+++ app/commands/cooldowns.js
@@ -14,6 +14,22 @@ module.exports = Base => class extends Base {
     }
 
     handleCooldowns(playerName) {
-        
+        //instantiate playerName variable if it hasn't been instantiated yet.
+        if (!this.onCooldownMap[playerName]) this.onCooldownMap[playerName] = {};
+
+        let cooldownArr = Object.values(this.onCooldownMap[playerName]);
+        let everyOutput = cooldownArr.every((i) => i === false);
+
+        console.log(cooldownArr, ' ', everyOutput)
+
+        if (everyOutput === true) {
+            return this.tellPlayer(playerName, 'All your commands are ready to use.', 'green')
+        };
+
+        for (let command in this.onCooldownMap[playerName]) {
+            if (typeof this.onCooldownMap[playerName][command] == 'number') {
+                this.cooldownCheck(`${command}`, playerName);
+            }  
+        }
     }
 }
