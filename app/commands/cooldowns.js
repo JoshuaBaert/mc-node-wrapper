3586b775d2a3e03ab4a6817741aa627ffb59f290
Harrison King <authorharrisonking@gmail.com>
2020-03-08 14:50:38 -0600

    improved by using only a single tellPlayerRaw

--- app/commands/cooldowns.js
+++ app/commands/cooldowns.js
@@ -24,10 +24,26 @@ module.exports = Base => class extends Base {
             return this.tellPlayer(playerName, 'All your commands are ready to use.', 'green')
         };
 
+        let combinedMessage = [{ text: `Commands on cooldown:\n`, color: 'green' },];
+
         for (let command in this.onCooldownMap[playerName]) {
+            let TimeStampMessage = [
+                { text: `${this.timeLeftMinutes(playerName, command)}`, color: 'white' },
+                { text: ` minutes and `, color: 'red' },
+                { text: `${this.timeLeftSeconds(playerName, command)}`, color: 'white' },
+                { text: ` seconds.\n`, color: 'red' },
+            ];
+    
+            let messageToSend = [[
+                { text: `!${command} `, color: 'white' },
+                { text: `will be available in `, color: 'red' }], ...TimeStampMessage,
+            ];
+
             if (typeof this.onCooldownMap[playerName][command] == 'number') {
-                this.cooldownCheck(`${command}`, playerName);
+                combinedMessage = [...combinedMessage, ...messageToSend]
             }  
         }
+
+        this.tellPlayerRaw(playerName, combinedMessage);
     }
 }
